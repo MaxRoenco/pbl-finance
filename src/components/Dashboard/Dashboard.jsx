122f79e069348c3c51f206285ded5a3a523d77ce
Roenco Maxim <maxroenco2004@gmail.com>
2024-09-19 09:07:20 +0300

    Fouth Commit URAAAA!

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,33 +3,40 @@ import styles from './Dashboard.module.css'
 const Dashboard = () => {
     return (
         <>
-            <div className={styles.layer1}>
+            {/* <div className={styles.layer1}>
                 <div className={styles.layer2}>
                     <div className={`${styles.greenLight} ${styles.topLeft}`}></div>
                     <div className={`${styles.greenLight} ${styles.topRight}`}></div>
                     <div className={`${styles.greenLight} ${styles.bottomLeft}`}></div>
                     <div className={`${styles.greenLight} ${styles.bottomRight}`}></div>
                 </div>
-            </div>
+            </div> */}
             <div className={styles.home}>
-                <div className={styles.container}>
+                <div className={styles.container1}>
                     <div className={styles.spendingsContainer}>
                         <p>TOTAL SPENDINGS</p>
                         <div className="">This week</div>
-                        <div className={styles.graph}>Graph</div>
                     </div>
                     <div className={styles.spendingsContainer}>
                         <p>TOTAL SPENDINGS</p>
                         <div className="">This week</div>
-                        <div className={styles.graph}>Graph</div>
                     </div>
                     <div className={styles.spendingsContainer}>
                         <p>TOTAL SPENDINGS</p>
                         <div className="">This week</div>
-                        <div className={styles.graph}>Graph</div>
                     </div>
                 </div>
-                <div className={styles.tradingViewContainer}>Graph</div>
+                <div className={styles.container2}>
+                    <div className={styles.graphContainer}>
+                        <div className={styles.graph}>Graph</div>
+                    </div>
+                    <div className={styles.newsContainer}>
+                        <p>News</p>
+                    </div>
+                </div>
+                <div className={styles.container3}>
+                    <p>Traiding Now</p>
+                </div>
             </div>
         </>
     );
