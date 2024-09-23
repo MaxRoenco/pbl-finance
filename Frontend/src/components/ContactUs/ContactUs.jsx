b59f95da268e96cc8135a05c0ce33cb4dbf11381
Artiom <artemilico1337@gmail.com>
2024-09-24 00:57:04 +0300

    Check Contact us Islam

--- Frontend/src/components/ContactUs/ContactUs.jsx
+++ Frontend/src/components/ContactUs/ContactUs.jsx
@@ -8,12 +8,12 @@ const ContactUs = () => {
                 <div className={styles.contactItem}>
                     <h2 className={styles.contactTitle}>Individuals and families</h2>
                     <p>Providing choice for those investing for retirement, a new home or a child’s education.</p>
-                  
+
                 </div>
                 <div className={styles.contactItem}>
                     <h2 className={styles.contactTitle}>Financial advisors</h2>
                     <p>Helping people at all income levels invest for their futures.</p>
-                  
+
                 </div>
                 <div className={styles.contactItem}>
                     <h2 className={styles.contactTitle}>Educational and nonprofit organizations</h2>
@@ -39,8 +39,8 @@ const ContactUs = () => {
                     <div className={styles.whoWeAreItem}>
                         <img src="../public/yellow-group.png" alt="people" className={styles.whoWeAreImage} />
                         <p className={styles.whoWeAreTitle}>19,000+ people </p>
-                        
-                       <p className={styles.whoWeAreText}> of different backgrounds, races, and nationalities.</p>
+
+                        <p className={styles.whoWeAreText}> of different backgrounds, races, and nationalities.</p>
                     </div>
                     <div className={styles.whoWeAreItem}>
                         <img src="../public/languages.png" alt="Multilingual Support" className={styles.whoWeAreImage} />
@@ -52,6 +52,32 @@ const ContactUs = () => {
                         <p className={styles.whoWeAreTitle}>42 countries</p>
                         <p className={styles.whoWeAreText}>enabling us to help more and more people.</p>
                     </div>
+                </div>
+            </div>
+            <div className={styles.feedbackContainer}>
+                <div className={styles.feedbackForm}>
+                    <input type="text" placeholder="Your Name" className={styles.inputField} />
+                    <input type="email" placeholder="Your Email" className={styles.inputField} />
+                    <textarea placeholder="Share your thoughts" className={styles.textArea}></textarea>
+                    <button className={styles.submitButton}>Share Your Feedback</button>
+                </div>
+                <div className={styles.contactUsText}>
+                    <img src="../public/contactus.png" alt="Contact Us" className={styles.contactImage} />
+                </div>
+            </div>
+
+            <div className={styles.contactInfoContainer}>
+                <div className={styles.contactInfo}>
+                    <h2 className={styles.contactHeading}>Call Us:</h2>
+                    <p className={styles.contactDetail}>+373 76 786 740</p>
+                </div>
+                <div className={styles.addressInfo}>
+                    <h2 className={styles.contactHeading}>Here’s Our Address:</h2>
+                    <p className={styles.addressDetail}>Forum Office Mt. Varlaam 63/23 str. Chisinau </p>
+                </div>
+                <div className={styles.emailInfo}>
+                    <h2 className={styles.contactHeading}>Send Us E-mail:</h2>
+                    <p className={styles.emailDetail}>investora@money.com</p>
                 </div>
             </div>
         </div>
