eaf6a1fb7dd40c84c2235b2cd4b3e4d4354931cc
MaxRoenco <maxroenco2004@gmail.com>
2024-11-08 19:23:34 +0200

    added faq page light mode

--- Frontend/src/components/FAQ/FAQ.jsx
+++ Frontend/src/components/FAQ/FAQ.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import styles from './FAQ.module.css';
 import { authContext } from "../../hooks/Context";
+import {faqs} from "../../constants/index";
 
 const FAQ = () => {
   let {lightMode, setLightMode} = useContext(authContext);
@@ -10,56 +11,8 @@ const FAQ = () => {
     setOpenIndex(openIndex === index ? null : index);
   };
 
-  const faqs = [
-    {
-      question: "What is a hedge fund?",
-      answer:
-        "A hedge fund is a pooled investment vehicle that uses a wide range of strategies to generate returns for its investors. These strategies may involve the use of leverage, derivatives, and other non-traditional assets to manage risk and seek opportunities for growth.",
-    },
-    {
-      question: "How is a hedge fund different from other investment funds?",
-      answer:
-        "Hedge funds typically have more flexibility than traditional funds, such as mutual funds. They can employ strategies like short selling, leverage, and derivatives, and they often invest in a wider range of asset classes, including stocks, bonds, commodities, and alternative assets (crowdfunds, businesses).",
-    },
-    {
-        question: "Who can invest in a hedge fund?",
-        answer:
-          "We are available to accredited investors, including high-net-worth individuals, institutions, and certain entities, due to the complex nature of the investments and the associated risks.",
-    },
-    {
-        question: "What types of strategies do hedge funds use?",
-        answer:
-          "We use various strategies, such as long/short equity, event driven, global macro, arbitrage, and managed futures, among others. Each strategy aims to take advantage of different market conditions to generate returns.",
-    },
-    {
-        question: "What are the risks associated with hedge funds?",
-        answer:
-          "It can involve significant risk due to their use of leverage, complex instruments, and speculative strategies. It's important to understand that there is a potential for loss, and these funds may not be suitable for all investors.",
-    },
-    {
-        question: "How do hedge funds manage risk?",
-        answer:
-          "We have sophisticated risk management techniques in place, such as diversification, hedging, and the use of stop-loss limits. However, even with these strategies, risks cannot be completely eliminated.",
-    },
-    {
-        question: "What are the fees associated with hedge funds?",
-        answer:
-          "We charge a management fee (usually around 2%) and a performance fee (around 20% of profits). These fees vary depending on the fund and the strategies employed.",
-    },
-    {
-        question: "How liquid are hedge fund investments?",
-        answer:
-          "We are less liquid than other types of investments. Investors may only be able to redeem their investment at certain times (e.g., quarterly or annually), depending on the fund's terms.",
-    },
-    {
-        question: "Can hedge funds guarantee returns?",
-        answer:
-          "No, We cannot guarantee returns. Their performance is subject to market conditions and the success of the investment strategies employed. Like all investments, hedge funds carry the risk of loss.",
-    },
-  ];
-
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${lightMode ? "bg-light-primary text-light-tertiary" : ""} `}>
       <h1 className={styles.heading}>FAQ</h1>
       {faqs.map((faq, index) => (
         <div
